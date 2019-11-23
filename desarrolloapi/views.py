7ed914e46d4d59ff929a940bd8330aa82b67c260
Joel Padilla <joe.padilla@alumnos.duoc.cl>
2019-11-23 00:03:06 -0300

    nuevo branch, guardar datos del modelo en BD

--- desarrolloapi/views.py
+++ desarrolloapi/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .forms import ContactoForm
 
 def home(request):
     return render(request,"index.html")
@@ -7,4 +8,15 @@ def galeria(request):
     return render(request,"galeria.html")
 
 def contacto(request):
-    return render(request,"contacto.html")
+    if request.method == 'POST':
+        form = ContactoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            toRender = render(request,"contacto.html")
+        else:
+            toRender = render(request,"contacto.html", {'ContactoForm': form})
+    else:
+        form = ContactoForm()
+        toRender = render(request,"contacto.html", {'ContactoForm': form })
+    
+    return toRender
